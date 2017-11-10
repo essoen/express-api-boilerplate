da0d59ef39a591f41e62bb0c729a88e25b5c1c01
Stein-Otto Svorstøl <steinotto@svorstol.com>
2017-11-10 16:17:48 +0100

    Exit process when loading is done
    Original implementation here: https://github.com/capraconsulting/dih-api/blob/dev/test/load.js

--- test/load.js
+++ test/load.js
@@ -1,3 +1,18 @@
+/* eslint-disable no-console */
 import { loadFixtures } from './helpers';
 
-loadFixtures();
+// verify that we are not in production
+if (process.env.NODE_ENV === 'production') {
+    console.log('Unable to load database in production (NODE_ENV==\'production\')');
+    process.exit(1);
+}
+
+loadFixtures()
+    .then(() => {
+        console.log('Loaded fixtures!');
+        process.exit(0);
+    })
+    .catch(err => {
+        console.log(err);
+        process.exit(1);
+});
